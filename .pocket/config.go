71ee6a3999f042b08a486b68c0eb9a105e7300f6
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-03-22 00:36:56 +0100

    feat(ci): add pocket

---
+++ .pocket/config.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/fredrikaverpil/pocket/pk"
+	"github.com/fredrikaverpil/pocket/tasks/github"
+	"github.com/fredrikaverpil/pocket/tasks/golang"
+)
+
+var Config = &pk.Config{
+	Auto: pk.Parallel(
+
+		// TODO: enable
+		// pk.WithOptions(
+		// 	markdown.Tasks(),
+		// 	pk.WithSkipPath("public"),
+		// ),
+
+		pk.WithOptions(
+			golang.Tasks(),
+			pk.WithDetect(golang.Detect()),
+			pk.WithSkipPath(`^\.$`, "public"),
+			pk.WithSkipTask(golang.Lint, "content/blog"),   // TODO: enable?
+			pk.WithSkipTask(golang.Format, "content/blog"), // TODO: enable?
+			pk.WithSkipTask(golang.Fix, "content/blog")),   // TODO: enable?
+
+		pk.WithOptions(
+			github.Tasks(),
+			pk.WithFlags(github.WorkflowFlags{
+				ReleasePleaseWorkflow: new(false),
+				StaleWorkflow:         new(false),
+				Platforms:             []github.Platform{github.Ubuntu},
+				ExternalWorkflows:     []string{"gh-pages.yml"},
+			}),
+		),
+	),
+	Manual: []pk.Runnable{
+		Serve,
+		Build,
+		Clean,
+	},
+}
